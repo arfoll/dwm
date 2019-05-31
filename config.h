95dd186992679e952e13135447f2dc005b73c157
Brendan Le Foll <brendan@fridu.org>
2019-05-31 10:13:52 +0200

    config.h: Remove steam & pidgin

--- config.h
+++ config.h
@@ -33,6 +33,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
 	{ "Firefox",  NULL,       NULL,       0,       False,       -1 },
+        { "Steam",  NULL,       NULL,       0,       True,       -1 },
 };
 
 /* layout(s) */
@@ -101,7 +102,6 @@ static Key keys[] = {
 	{ Mod4Mask,                     XK_t,      spawn,          {.v = termcmdr } },
 	{ Mod4Mask,                     XK_f,      spawn,          {.v = firefox } },
 	{ Mod4Mask,                     XK_c,      spawn,          {.v = chromium } },
-	{ Mod4Mask,                     XK_p,      spawn,          {.v = pidgin } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
