df6d56911af5fb0275a2610209b0fdf71acb070e
Brendan Le Foll <brendan@fridu.org>
2019-06-03 11:16:46 +0200

    config.h: Add wine to rules
    Signed-off-by: Brendan Le Foll <brendan@fridu.org>

--- config.h
+++ config.h
@@ -32,6 +32,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
+	{ "Wine",     NULL,       NULL,       0,            True,        -1 },
 	{ "Firefox",  NULL,       NULL,       0,       False,       -1 },
 };
 
